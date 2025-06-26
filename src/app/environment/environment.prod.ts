9c973621354ee8196e3e0ddb1d963ec7fcce03ad
git-michael-hub <dev.michael.ph@gmail.com>
2025-06-27 04:10:44 +0800

    Fix

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
   debugMode: false,
+  projectId: "",
+  appId: "",
+  storageBucket: "",
+  apiKey: "",
+  authDomain: "",
+  messagingSenderId: ""
 };
