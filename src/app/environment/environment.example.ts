51dbeefd87cc98482f0b9b8d0e2cc7a1c5884f2a
git-michael-hub <dev.michael.ph@gmail.com>
2025-06-27 04:13:48 +0800

    Fix

---
+++ src/app/environment/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  debugMode: true,
+  projectId: "workie-work-a95d3",
+  appId: "1:699895841525:web:f17bb3776b80f583c67bbc",
+  storageBucket: "workie-work-a95d3.firebasestorage.app",
+  apiKey: "AIzaSyBpoK0x1oli4pjWhOu5bKegLwi72jWBgco",
+  authDomain: "workie-work-a95d3.firebaseapp.com",
+  messagingSenderId: "699895841525"
+};
